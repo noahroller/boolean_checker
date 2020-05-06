bac24fa6225dea67c91c9e6288b77d48af98dbc3
Noah Roller <rollernoah@gmail.com>
2020-05-06 01:56:19 -0400

    Added gitignore and cleaned eq

--- boolean_eq.py
+++ boolean_eq.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3.7
-from boolean_simp_v2 import simplify
+from boolean_simp import simplify
 
 simplify("a+b+c*d", values)
 
-#comment added to change file for new commit
-#adding another comment for learning git
-#variables_l = list(set(expression))
+expression = ''
+variables_l = list(set(expression))
